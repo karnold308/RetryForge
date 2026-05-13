dfa587f8390e36c69f5c196a5f76d46bb7a4d070
karnold308 <karnold308@gmail.com>
2026-05-13 17:25:59 -0400

    working on db connection for register new user

--- server/model/User.js
+++ server/model/User.js
@@ -1,5 +1,4 @@
-const user = process.env.NODE_ENV === 'production' ? process.env.retryforge_PGHOST : process.env.DB_USER;
-
+const user = process.env.NODE_ENV === 'production' ? process.env.retryforge_PGUSER : process.env.DB_USER;
 const pwd = process.env.NODE_ENV === 'production' ? process.env.retryforge_PGPASSWORD : encodeURIComponent(process.env.DB_PWD) ; 
 const host = process.env.NODE_ENV === 'production' ? process.env.retryforge_PGHOST : process.env.DB_HOST;
 const dbName = process.env.NODE_ENV === 'production' ? process.env.retryforge_PGDATABASE : process.env.DB_NAME;
