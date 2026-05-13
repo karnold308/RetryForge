5f22599d3d8d615d527d2e564ee1eb0c949b8b91
karnold308 <karnold308@gmail.com>
2026-05-13 16:08:31 -0400

    getting back end urls setup

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,8 +1,8 @@
 // remove localhost urls and '!origin' condition below
 const allowedOrigins = [
-    "https://www.yourdomain.com", 
-    "www.yoursite.com", 
-    "yoursite.com", 
+    "https://www.retryforge.com", 
+    "www.retryforge.com", 
+    "retryforge.com", 
     "http://127.0.0.1:5500", 
     "http://localhost:3500",
     "http://localhost:5173",
