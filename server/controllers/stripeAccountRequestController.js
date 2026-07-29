ed36b350c23ba6f0c89838fbf78561342407c56d
karnold308 <karnold308@gmail.com>
2026-07-29 16:49:58 -0400

    reverting test change to stripe connect timeout jwt.

--- server/controllers/stripeAccountRequestController.js
+++ server/controllers/stripeAccountRequestController.js
@@ -4,7 +4,7 @@ export function createStripeState(userId) {
     return jwt.sign(
         { userId },
         process.env.JWT_SECRET,
-        { expiresIn: "10s" }
+        { expiresIn: "1h" }
     )
 }
 
