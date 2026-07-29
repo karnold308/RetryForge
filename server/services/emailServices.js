7efbfdda7ec77bd30e7afa843a88225464a83837
karnold308 <karnold308@gmail.com>
2026-07-29 10:57:54 -0400

    debugging emails.

--- server/services/emailServices.js
+++ server/services/emailServices.js
@@ -1,4 +1,5 @@
 import { Resend } from 'resend'
+console.log(process.env.RESEND_API_KEY);
 const resend = new Resend(process.env.RESEND_API_KEY)
 
 console.log("RESEND_API_KEY exists:", !!process.env.RESEND_API_KEY)
